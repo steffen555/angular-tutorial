7b48ec468c56edd46cc9edc750cb60894a430f92
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2019-04-10 11:10:28 +0200

    Completed #9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,20 @@ import {Component} from '@angular/core';
       Welcome to {{ title }}!
     </h1>
     <app-input-button-unit></app-input-button-unit>
+    <ul>
+      <li *ngFor="let todoItem of todoList">
+        {{ todoItem.title }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'app';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'lav mad'},
+    {title: 'hop i sengen'},
+    {title: 'gå på wc'}
+  ];
 }
